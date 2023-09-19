497f2a08399194cf2d6006ebe8f34b0c06333038
pavlovdog <pavlovdog.moscow@gmail.com>
2023-09-19 12:12:16 +0400

    Update envs

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,7 +2,7 @@ import rest from '@feathersjs/rest-client';
 import { createClient } from '@vibeproof/api';
 
 
-const connection = rest(process.env.API_URL || 'http://localhost:3030')
+const connection = rest(process.env.REACT_APP_API_URL || 'http://localhost:3030')
     .fetch(window.fetch.bind(window));
 
 export const client = createClient(connection);
